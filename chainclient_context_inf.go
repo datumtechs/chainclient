67fa3a14c6048c36da1a37b53efb5e2198e2fbe6
bglmmz <lvxiaoyi@juzix.net>
2022-07-05 18:37:56 +0800

    fix function parameters

--- chainclient_context_inf.go
+++ chainclient_context_inf.go
@@ -12,6 +12,6 @@ type Context interface {
 	GetPrivateKey() *ecdsa.PrivateKey
 	GetWalletAddress() common.Address
 	GetClient() *ethclient.Client
-	EstimateGas(from, to common.Address, data []byte, gas uint64, gasPrice *big.Int) (uint64, error)
-	BuildTxOpts(uint64, gasLimit uint64) (*bind.TransactOpts, error)
+	EstimateGas(from common.Address, to *common.Address, data []byte, gas uint64, gasPrice *big.Int) (uint64, error)
+	BuildTxOpts(value, gasLimit uint64) (*bind.TransactOpts, error)
 }
