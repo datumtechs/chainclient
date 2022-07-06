cd4ae3554228e00a8ec17e91c1373efcf9185647
bglmmz <lvxiaoyi@juzix.net>
2022-07-06 12:23:48 +0800

    add GetReceipt() use context's wallet address as default caller

--- ethclient_context.go
+++ ethclient_context.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
@@ -51,8 +52,10 @@ func (ctx *EthContext) GetWalletAddress() common.Address {
 func (ctx *EthContext) GetClient() *ethclient.Client {
 	return ctx.client
 }
-func (ctx *EthContext) EstimateGas(from common.Address, to *common.Address, input []byte, gas uint64, gasPrice *big.Int) (uint64, error) {
-	msg := ethereum.CallMsg{From: from, To: to, Data: input, Gas: gas, GasPrice: gasPrice}
+
+// EstimateGas uses context's wallet address as the caller (from)
+func (ctx *EthContext) EstimateGas(to *common.Address, input []byte, gas uint64, gasPrice *big.Int) (uint64, error) {
+	msg := ethereum.CallMsg{From: ctx.walletAddress, To: to, Data: input, Gas: gas, GasPrice: gasPrice}
 	timeout := time.Duration(500) * time.Millisecond
 	backendCtx, cancelFn := context.WithTimeout(context.Background(), timeout)
 	defer cancelFn()
@@ -86,4 +89,41 @@ func (ctx *EthContext) BuildTxOpts(value, gasLimit uint64) (*bind.TransactOpts, 
 	txOpts.GasLimit = gasLimit                   // in units
 	txOpts.GasPrice = gasPrice
 	return txOpts, nil
+}
+
+func (ctx *EthContext) GetReceipt(timeoutCtx context.Context, txHash common.Hash, interval time.Duration) *types.Receipt {
+	fetchReceipt := func(txHash common.Hash) (*types.Receipt, error) {
+		receipt, err := ctx.client.TransactionReceipt(context.Background(), txHash)
+		if nil != err {
+			//including NotFound
+			log.Printf("failed to get transaction receipt, txHash: %s", txHash.Hex())
+			return nil, err
+		} else {
+			log.Printf("transaction receipt: %#v", receipt)
+			return receipt, nil
+		}
+	}
+
+	if interval < 0 { // do once only
+		receipt, err := fetchReceipt(txHash)
+		if nil != err {
+			return nil
+		}
+		return receipt
+
+	} else {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-timeoutCtx.Done():
+				log.Printf("get transaction receipt timeout, txHash: %s", txHash.Hex())
+				return nil
+			case <-ticker.C:
+				if receipt, err := fetchReceipt(txHash); nil == err {
+					return receipt
+				}
+			}
+		}
+	}
 }
